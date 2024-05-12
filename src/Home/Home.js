4f4eb8dcaec9567a0cc5c85483df628463ccfd22
AntonCSalvador <antoncsalvador@gmail.com>
2024-05-12 02:02:28 -0400

    added svg music button

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,24 +1,36 @@
 import { useState, useRef, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-import "./Home.css"
-import Navbar from "../Navbar/Navbar" 
+import "./Home.css";
+import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
     const navigate = useNavigate();
-    const [isMuted, setIsMuted] = useState(false); // State to track mute
+    const [isMuted, setIsMuted] = useState(true); // Initialize as muted
     const audioRef = useRef(null); // Ref for the audio element     
+    const [videoPlaying, setVideoPlaying] = useState(false);
 
     const toggleMute = () => {
         setIsMuted(!isMuted);
         if (audioRef.current) {
-            audioRef.current.muted = !audioRef.current.muted;
+            audioRef.current.muted = !audioRef.current.muted; // Toggle the muted property
         }
     };
 
     useEffect(() => {
+        const videoElement = document.querySelector('video');
+        if (videoElement) {
+            videoElement.onplay = () => {
+                setVideoPlaying(true);
+            };
+            videoElement.onerror = () => {
+                setVideoPlaying(false);
+            };
+        }
         if (audioRef.current) {
             audioRef.current.volume = 0.5; // Set default volume to 50%
-            audioRef.current.play();
+            audioRef.current.play().catch(err => {
+                console.error("Error playing audio:", err);
+            });
         }
     }, []);
 
@@ -26,12 +38,15 @@ const Home = () => {
         <div className="home-page-container">
             <Navbar />
 
-            <div className="picture-carousel">
-                <div className="temp-centered-text">Game opening screen</div>
+            <div class="picture-carousel">
+                <video class="w-full h-full" controls loop autoplay>
+                    <source src="/main_menu.mp4" type="video/mp4"/>
+                    Your browser does not support this video.
+                </video>
             </div>
 
             <div className="picture-carousel">
-            <div className="temp-centered-text">Picture carousel</div>
+                <div className="temp-centered-text">Picture carousel</div>
             </div>
 
             <div className="download-title">
@@ -39,37 +54,46 @@ const Home = () => {
             </div>
 
             <div className="download-links-box">
-                <div class="flex w-3/12 items-center justify-center">
-                    <div class="text-2xl m-4 w-1/2 text-center">Mac</div>
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4">
+                <div className="flex w-3/12 items-center justify-center">
+                    <div className="text-2xl m-4 w-1/2 text-center">Mac</div>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4">
                          <a href="https://drive.google.com/uc?export=download&id=1lHRB4EOvokiN0LybZw75ij7rI1a_WWZ9">Download</a>
                     </button>
                 </div>
 
-                <div class="flex w-3/12 items-center justify-center">
-                    <div class="text-2xl m-4 w-1/2 text-center">Linux</div>
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4">
+                <div className="flex w-3/12 items-center justify-center">
+                    <div className="text-2xl m-4 w-1/2 text-center">Linux</div>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4">
                          <a href="https://drive.google.com/uc?export=download&id=1lHRB4EOvokiN0LybZw75ij7rI1a_WWZ9">Download</a>
                     </button>
                 </div>
 
-                <div class="flex w-3/12 items-center justify-center">
-                    <div class="text-2xl m-4 w-1/2 text-center">Windows</div>
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4">
+                <div className="flex w-3/12 items-center justify-center">
+                    <div className="text-2xl m-4 w-1/2 text-center">Windows</div>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4">
                          <a href="https://drive.google.com/uc?export=download&id=1lHRB4EOvokiN0LybZw75ij7rI1a_WWZ9">Download</a>
                     </button>
                 </div>
             </div>
 
-            <button onClick={toggleMute} className="fixed bottom-4 right-4 bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded">
-                {isMuted ? 'Unmute' : 'Mute'}
-            </button>
-            
-            <audio ref={audioRef} src="main_menu.mp3" loop />
+            <button onClick={toggleMute} className="fixed bottom-4 right-4 bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded flex items-center justify-center rounded">
+            {isMuted ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 stroke-black">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553Z" />
+                        <path strokeLinecap="round" d="M3 3l18 18" />
+                    </svg>
+                ) : (
 
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 stroke-black">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553Z" />
+                    </svg>
+                )}
+            </button>            
 
+            <audio ref={audioRef} src="main_menu.mp3" loop muted />
         </div>
-    )
-} 
+    );
+};
 
-export default Home
+export default Home;
+
