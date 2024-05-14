93402a3e4a3dad1effb7c769bff01a394bd4af47
AntonCSalvador <antoncsalvador@gmail.com>
2024-05-14 03:02:03 -0400

    added purple borders

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="header-container flex flex-row justify-between w-full">
+        <div className="header-container flex flex-row justify-between w-full border-b-4 border-custom-purple">
             <img src="https://docs.ufosc.org/img/logo.png" class="ml-4 w-12 h-12 items-left justify-left cursor-pointer" onClick={ () => window.open('https://ufosc.org/', '_blank')}/>
-            <div class="flex justify-end flex-grow">
+            <div class="flex justify-end flex-grow items-center">
             <div className="header-option-container" onClick={() => {navigate(`/download`)}}>
                 <div className="header-option-text">Download</div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-purple': '#9C4796',  // You can name this key whatever you like
+      },
+      borderColor: theme => ({
+        ...theme('colors'),
+        'custom-purple': '#9C4796',  // Reusing the custom color for border
+      })
+    }
   },
   plugins: [],
 }
