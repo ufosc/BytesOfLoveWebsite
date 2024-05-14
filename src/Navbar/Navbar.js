fd4bf4050aef7689425fede1178d1735f1361bb1
AntonCSalvador <antoncsalvador@gmail.com>
2024-05-14 03:09:24 -0400

    lowered navbar border

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="header-container flex flex-row justify-between w-full border-b-4 border-custom-purple">
+        <div className="header-container flex flex-row justify-between w-full border-b-2 border-custom-purple">
             <img src="https://docs.ufosc.org/img/logo.png" class="ml-4 w-12 h-12 items-left justify-left cursor-pointer" onClick={ () => window.open('https://ufosc.org/', '_blank')}/>
             <div class="flex justify-end flex-grow items-center">
             <div className="header-option-container" onClick={() => {navigate(`/download`)}}>
