64773723bfb373c81d4e91a4f4f03ad7ba66d353
xiaoguo-jia <xiaoguojia@ufl.edu>
2024-06-19 12:52:02 -0400

    added logo and containers for elements in header

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,14 +5,24 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="header-container flex flex-row w-full border-b-2 items-center border-custom-purple bg-[#fccbff] justify-between">
-      <img
-        src="https://docs.ufosc.org/img/logo.png"
-        class="ml-4 w-12 h-12 items-left content-center cursor-pointer"
-        onClick={() => window.open("https://ufosc.org/", "_blank")}
-        alt="UF OSC Logo"
-      />
-      <div class="flex flex-row justify-evenly items-center sm:gap-16 px-10 gap-6">
+    <div className="header-container flex flex-row w-full border-b-2 items-center border-custom-purple bg-[#fccbff]">
+      <div className="logo-container flex flex-row items-center">
+        <img
+            src="https://docs.ufosc.org/img/logo.png"
+            class="logo"
+            onClick={() => window.open("https://ufosc.org/", "_blank")}
+            alt="UF OSC Logo"
+        />
+        <img 
+            src="/BoL Logo Edited.png" 
+            className="logo" 
+            alt="BoL Logo" 
+            onClick={() => window.open('https://github.com/ufosc/VisualNovel/https://ufosc.org/', '_blank')}
+        />
+        </div>
+        
+
+      <div class="flex flex-row justify-evenly items-center sm:gap-16 px-10 gap-6 ml-auto">
         <div
           className="header-option-container"
           onClick={() => {
